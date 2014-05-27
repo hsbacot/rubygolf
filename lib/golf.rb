dc604af9b2479b12d09fbdfb767b525474240cb6
Edmund Mai <edmundmai@gmail.com>
2014-05-28 07:52:59 +0800

    hole4

--- lib/golf.rb
+++ lib/golf.rb
@@ -24,4 +24,16 @@ class Golf
   def self.hole8(n)
     a=[1,1];(1..n-2).map{|i|a<<a[-1]+a[-2]}
   end
+  
+  def self.hole4(a)
+    r = []
+    a.each do |e|
+      n = "hat(#{e})" if e =~ /man/
+      n = "#{e[0..-2]}(bone))" if e =~ /dog/
+      n = e.sub("cat", "dead") if e =~ /cat/
+      r << n
+    end
+    r
+  end
 end
+
