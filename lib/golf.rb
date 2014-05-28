1c4cf93c1cf372aaab324c9d11910f80a4fa009d
ss <git@push.com>
2014-05-27 20:17:26 -0400

    hole 9

--- lib/golf.rb
+++ lib/golf.rb
@@ -8,12 +8,17 @@ class Golf
     1.upto(a.length) { |n| a.each_cons(n) { |c| x << c } }
     x
   end
-  
+
   def self.hole2
     ("a".."z").to_a
   end
 
   def self.hole3(n)
     (1..n).to_a.reduce(1){|i,a|a*i}
+  end
+
+  def self.hole9(s)
+    s.gsub!(/[a-zA-z]{10,}/) { |w| w[0..3] + "..." + w[-3..-1] } if s.size > 10
+    s
   end
 end
